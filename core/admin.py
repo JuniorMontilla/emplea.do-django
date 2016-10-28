bd9fa4f0459e3744e62d5087fd1a4b14ac552d2e
Junior Montilla <juniormontillaacosta@gmail.com>
2016-10-28 17:06:21 -0400

    clean the repos

--- core/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from models import Job, Category
-
-class CategoryAdmin(admin.ModelAdmin):
-	prepopulated_fields = {"slug": ("name",)}
-admin.site.register(Job)
-admin.site.register(Category, CategoryAdmin)
